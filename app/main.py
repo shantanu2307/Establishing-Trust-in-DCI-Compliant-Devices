364d45fb5b3ea4b8e103157ca53698c2f194fd47
Shantanu Goyal <shantanugoyal23@gmail.com>
2023-02-10 08:20:26 +0530

    refactor: tabs size 4

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import request, jsonify, g
 from flask_cors import CORS
 import logging
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-  app.logger.debug("Shantanu")
-  return "Hello World!",200
+    app.logger.debug("Shantanu")
+    return "Hello World!", 200
