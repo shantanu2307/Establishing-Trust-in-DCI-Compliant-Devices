feb7c24ed86cb2888488628c5134b141af3147aa
Shantanu Goyal <shantanugoyal23@gmail.com>
2023-02-10 08:16:59 +0530

    feat: added mongodb logic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request, jsonify, g
+from flask_cors import CORS
 import logging
 app=Flask(__name__)
+CORS(app)
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
---
+++ models/main.py
@@ -0,0 +1,93 @@
+from factory.validation import Validator
+from factory.database import Database
+
+
+class TheatreOwner(object):
+    def __init__(self):
+        self.validator = Validator()
+        self.db = Database()
+        self.collection_name = "theatre_owners"
+        self.fields = {
+            "name": "string",
+            "email": "string",
+            "password": "string",
+            "theatre_id": "string",
+            "location": "string",
+            "showtimes": ["string"],
+            "theatre_capacity": "int",
+            "created": "datetime",
+            "updated": "datetime"
+        }
+
+        self.create_required_fields = [
+            "name", "email", "password", "theatre_id", "location", "showtimes", "theatre_capacity"]
+        self.create_optional_fields = []
+
+        self.update_required_fields = [
+            "name", "email", "password", "theatre_id", "location", "showtimes", "theatre_capacity"]
+        self.update_optional_fields = []
+
+    def create(self, theatre_owner):
+        self.validator.validate(
+            theatre_owner, self.fields, self.create_required_fields, self.create_optional_fields)
+        res = self.db.insert(theatre_owner, self.collection_name)
+        return "Inserted Id " + res
+
+    def find(self, theatre_owner):
+        return self.db.find(theatre_owner, self.collection_name)
+
+    def find_by_id(self, theatre_owner_id):
+        return self.db.find_by_id(theatre_owner_id, self.collection_name)
+
+    def update(self, theatre_owner_id, theatre_owner):
+        self.validator.validate(
+            theatre_owner, self.fields, self.update_required_fields, self.update_optional_fields)
+        return self.db.update(theatre_owner_id, theatre_owner, self.collection_name)
+
+    def delete(self, theatre_owner_id):
+        return self.db.delete(theatre_owner_id, self.collection_name)
+
+
+class Certificate(object):
+    def __init__(self):
+        self.validator = Validator()
+        self.db = Database()
+        self.collection_name = "certificates"
+        self.fields = {
+            "theatre_id": "string",
+            "device_id": "string",
+            "server_id": "string",
+            "hashed_key": "string",
+            "created": "datetime",
+            "updated": "datetime"
+        }
+
+        self.create_required_fields = [
+            "theatre_id", "device_id", "server_id", "hashed_key"]
+
+        self.create_optional_fields = []
+
+        self.update_required_fields = [
+            "theatre_id", "device_id", "server_id", "hashed_key"]
+
+        self.update_optional_fields = []
+
+    def create(self, certificate):
+        self.validator.validate(
+            certificate, self.fields, self.create_required_fields, self.create_optional_fields)
+        res = self.db.insert(certificate, self.collection_name)
+        return "Inserted Id " + res
+
+    def find(self, certificate):
+        return self.db.find(certificate, self.collection_name)
+
+    def find_by_id(self, certificate_id):
+        return self.db.find_by_id(certificate_id, self.collection_name)
+
+    def update(self, certificate_id, certificate):
+        self.validator.validate(
+            certificate, self.fields, self.update_required_fields, self.update_optional_fields)
+        return self.db.update(certificate_id, certificate, self.collection_name)
+
+    def delete(self, certificate_id):
+        return self.db.delete(certificate_id, self.collection_name)
