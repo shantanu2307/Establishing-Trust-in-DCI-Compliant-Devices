840fdb80654bb670b92b5753a4d44f86c18f4cec
Shantanu Goyal <shantanugoyal23@gmail.com>
2023-03-17 15:00:20 +0530

    dev: controllers modified #2

--- tests/decrypt.py
+++ tests/decrypt.py
@@ -12,8 +12,12 @@ def decrypt():
         kdm = _kdm.read()
     with open("./data/private_key.pem", "rb") as _private_key:
         private_key = serialization.load_pem_private_key(_private_key.read(), password=None, backend=default_backend())
+    hex_kdm = kdm.hex()
+    new_kdm = bytes.fromhex(hex_kdm)
+    print(new_kdm)
+
     decrypted_kdm = private_key.decrypt(
-        kdm,
+        new_kdm,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
